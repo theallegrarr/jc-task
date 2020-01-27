6f46bf4dd3d9a75469b4b1741aafe3d32f7c0671
theallegrarr <nurudeen.amedu@gmail.com>
2020-01-27 23:41:30 +0100

    feature: cta content added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import Header from './components/Header';
+import Landing from './components/Landing';
 import './css/index.css';
 
 function App() {
   return (
     <div className="main-container">
       <Header />
+      <Route 
+        exact path='/'
+        render={props => {
+            return (<Landing {...props}/>)
+        }} />
     </div>
   );
 }
