b86484f4b10b71ec4e42f53a7c4e06424003e658
theallegrarr <nurudeen.amedu@gmail.com>
2020-01-30 13:38:03 +0100

    style: styling improved

--- src/components/Profile/Password.js
+++ src/components/Profile/Password.js
@@ -7,25 +7,31 @@ export default function SetAnswers({ form, setForm }) {
   }
 
   return(
-    <>
-      <label>Current Password</label>
-      <input 
-      type='password'
-      name='old_password'
-      onChange={(e) => updateForm(e.target.name, e.target.value)}
-      value={form.old_password}></input>
-      <label>New Password</label>
-      <input 
-      type='password'
-      name='new_password'
-      onChange={(e) => updateForm(e.target.name, e.target.value)}
-      value={form.new_password}></input>
-      <label>Confirm New Password</label>
-      <input 
-      type='password'
-      name='confirm_new_password'
-      onChange={(e) => updateForm(e.target.name, e.target.value)}
-      value={form.confirm_new_password}></input>
-    </>
+    <div className='set-password'>
+      <div>
+        <label>Current Password</label>
+        <input 
+        type='password'
+        name='old_password'
+        onChange={(e) => updateForm(e.target.name, e.target.value)}
+        value={form.old_password}></input>
+      </div>
+      <div>
+        <label>New Password</label>
+        <input 
+        type='password'
+        name='new_password'
+        onChange={(e) => updateForm(e.target.name, e.target.value)}
+        value={form.new_password}></input>
+        <label>Confirm New Password</label>
+      </div>
+      <div>
+        <input 
+        type='password'
+        name='confirm_new_password'
+        onChange={(e) => updateForm(e.target.name, e.target.value)}
+        value={form.confirm_new_password}></input>
+      </div>
+    </div>
   )
 }
